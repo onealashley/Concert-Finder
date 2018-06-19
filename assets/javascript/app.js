245c59442a38590250a16e6160b8c5050d793876
ashley o <oneal.ashleyc@gmail.com>
2018-06-18 21:16:44 -0400

    mondays class.... added some styling, and fixed some javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 function displayConcert(location) {
-    var queryUrl = "https://api.eventful.com/json/events/search?location=" + location + "&cors_filter=1&t=this+month&category=music&app_key=FFR9NRDxgF32bNs5";
+    var queryUrl = "https://api.eventful.com/json/events/search?location=" + location + "&tag=music&cors_filter=1&t=this+month&c=music&app_key=FFR9NRDxgF32bNs5";
 
     $.ajax({
         url: queryUrl,
@@ -8,16 +8,18 @@ function displayConcert(location) {
         method: "GET"
     }).then(function(response) {
         console.log(response.events);
-        for (i = 0; i <response.events.event.length; i++) {
+        for (i = 0; i < response.events.event.length; i++) {
             console.log(response.events.event[i]);
-            var concertInfo = $("<div>");
-            var title = $("<p>").text(response.events.event[i].title);
-            var venue = $("<p>").text(response.events.event[i].venue_name);
+            var concertInfo = $("<div class=events>");
+            var title = $("<p id = artist>").text(response.events.event[i].title);
+            // $("#artist").click(function());
+            var venue = $("<p id = venue>").text(response.events.event[i].venue_name);
+            // $("#venue").click(funtcion());
             var address = $("<p>").text(response.events.event[i].venue_address);
-            var startDate = $("<p>").text(response.events.event[i].start_time);
-            concertInfo.append(title).append(venue).append(address).append(startDate);
+            var parsedDate = $("<div class=date>").append(moment(response.events.event[i].start_time).format('Do MMM'));
+            concertInfo.append(title).append(venue).append(address);
             
-            $("#event-view").append(concertInfo);
+            $("#event-view").append(parsedDate).append(concertInfo);
             
         }
     });
